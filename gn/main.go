c0f5949b03e0c918c4e810cc2071b2026a58e816
ziemen <173351113+ziemen4@users.noreply.github.com>
2024-07-13 00:37:07 +0200

    WIP: Sending RawData correctly

--- gn/main.go
+++ gn/main.go
@@ -133,24 +133,43 @@ type tronBlockCircuit struct {
 	//NewBlockID  [32]byte
 	//PrevBlockID [32]byte
 	PublicKey   ecdsa.PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]
-	RawData     []byte
+	RawData     []uints.U8
 	Signature   ecdsa.Signature[emulated.Secp256k1Fr]
 	//TxRoot      [32]byte
 }
 
 func (c *tronBlockCircuit) Define(api frontend.API) error {
 	// Expected hash
-	hash := sha256.Sum256(c.RawData)
+	fmt.Println("RawData: ", c.RawData)
+	fmt.Println("PublicKey: ", c.PublicKey)
+	fmt.Println("Signature: ", c.Signature)
+
+	//rawDataBytes := make([]byte, len(c.RawData))
+	//for i, u8 := range c.RawData {
+	//	rawDataBytes[i] = u8.Val.(byte)
+	//}
+
+	// Hardcode the actual value of the hash until conversion from c.RawData can be done
+	rawDataBytes, err := hex.DecodeString("08b0e49e9a853212206ef000a8a81ddae3419c56f9b1ba01ae1215aeffbb3865f9e8b73495c29e41e41a200000000003bffa8b92248ec47500deaedcbf8c3e20bdcaa058e5716b858ddb50388cf5ff1d4a15412a4d700c196a78f8ff7f0bf17d93fe6018396d2e501e")
+	if err != nil {
+		return err
+	}
+	hash := sha256.Sum256(rawDataBytes)
 
 	// Create circuit that checks that sha256(In) == Expected
 	hashCircuit := sha2Circuit{
-		In:       uints.NewU8Array(c.RawData),
+		In:       c.RawData,
 		Expected: [32]uints.U8(uints.NewU8Array(hash[:])),
 	}
 	hashCircuit.Define(api)
 
 	// Use m as a message in the field of the curve
 	msg := emulated.ValueOf[emulated.Secp256k1Fr](sigEcdsa.HashToInt(hash[:]))
+	// 266ced7e1b0899f39771c0a9bf1e6cdfb282c7719d44592e2f877cda60099b2a
+	// Print hash as hex
+	fmt.Println("hash: ", hex.EncodeToString(hash[:]))
+	fmt.Println("hash to int: ", sigEcdsa.HashToInt(hash[:]))
+	fmt.Println("msg: ", msg)
 
 	// Check that msg == hash % N
 	// N := ecdsa.GetCurveOrder(api, sw_emulated.GetSecp256k1Params())
@@ -174,6 +193,8 @@ func main() {
 	r.SetBytes(blocks[0].Signature[:32])
 	s.SetBytes(blocks[0].Signature[32:64])
 
+	input := blocks[0].RawData
+
 	assignment := tronBlockCircuit{
 		//NewBlockID:  [32]byte(blocks[0].NewBlockID),
 		//PrevBlockID: [32]byte(blocks[0].PrevBlockID),
@@ -181,13 +202,17 @@ func main() {
 			X: emulated.ValueOf[emulated.Secp256k1Fp](pubx),
 			Y: emulated.ValueOf[emulated.Secp256k1Fp](puby),
 		},
-		RawData: blocks[0].RawData,
+		RawData: uints.NewU8Array(input),
 		Signature: ecdsa.Signature[emulated.Secp256k1Fr]{
 			R: emulated.ValueOf[emulated.Secp256k1Fr](r),
 			S: emulated.ValueOf[emulated.Secp256k1Fr](s),
 		},
 		//TxRoot:    [32]byte(blocks[0].TxRoot),
 	}
+
+	fmt.Println("assignment PK: ", assignment.PublicKey)
+	fmt.Println("assignment RD: ", assignment.RawData)
+	fmt.Println("assignment SIG: ", assignment.Signature)
 
 	println("Assigning witness...")
 	witness, err := frontend.NewWitness(&assignment, ecc.BN254.ScalarField())
@@ -196,7 +221,12 @@ func main() {
 	}
 	println("Witness assigned")
 
-	var circuit tronBlockCircuit
+	circuit := tronBlockCircuit{
+		// This is neccessary to avoid uninitialized slice error
+		// See: https://github.com/Consensys/gnark/issues/970
+		RawData: make([]uints.U8, len(input)),
+	}
+
 	println("Compiling circuit...")
 	cs, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit)
 	if err != nil {
